717d17d0743ffaa7158b6629c0d87ff175ab80bd
luanmgdo <luanmacedo108@gmail.com>
2020-07-08 19:29:39 -0300

    mostrar horas

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import datetime
 from telegram.ext import Updater, CommandHandler
 
 def welcome(update, context):
@@ -5,11 +6,25 @@ def welcome(update, context):
     print(message)
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
+def agora(update, context):
+    now = datetime.datetime.now()
+    time_stamp = "Hora atual: {}/{}/{} - {}:{}:{}" . format(
+        now.day,
+        now.month,
+        now.year,
+        now.hour,
+        how.minute,
+        now.second
+    )
+    print(time_stamp)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=time_stamp)
+
 def main():
     token = '1179070933:AAGW5gDZ-1FCECKV1M0q4DLxLR8hoLWNrUQ'
     updater = Updater(token=token, use_context=True)
 
     updater.dispatcher.add_handler(CommandHandler('start', welcome))
+    updater.dispatcher.add_handler(CommandHandler('agora', agora))
 
     updater.start_polling()
     print(str(updater))
